5bdb3c625ce30b38ee5a9b9fe44632b93ee7c36d
Fredrik <fredrik.melander@gmail.com>
2016-01-31 13:10:30 +0100

    move the random stuff out of sequence.py to random.py

--- iternity/random.py
+++ iternity/random.py
@@ -9,3 +9,23 @@ def coinflip():
 def choicev(*items):
     """Variadic versions of random.choice"""
     return random.choice(items)
+
+
+def random_numbers(start, stop):
+    """Generates an endless stream of random numbers in the given range"""
+    while True:
+        yield random.randrange(start, stop)
+
+
+def random_numbers_unique(start, stop):
+    """Generates a stream of numbers in the given range where each number
+    is guaranteed to only show up once"""
+    taken = []
+    total_count = stop - start
+    while True:
+        if len(taken) == total_count:
+            return
+        num = random.randrange(start, stop)
+        if num not in taken:
+            taken.append(num)
+            yield num
